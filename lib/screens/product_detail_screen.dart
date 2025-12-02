31b4f4e6a009067636c14ecc1c7ffd06f59f23d2
bytefi.sh <up2279917@myport.ac.uk>
2025-12-02 20:31:30 +0000

    Add more details to product screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -9,26 +9,62 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final String? productId =
-        ModalRoute.of(context)?.settings.arguments as String?;
+    final String productId =
+        ModalRoute.of(context)?.settings.arguments as String;
 
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
           children: [
             const AppHeader(),
             Container(
               color: Colors.white,
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+              padding: const EdgeInsets.all(40),
               child: FutureBuilder<Product?>(
-                future: loadProduct(productId ?? ''),
+                future: loadProduct(productId),
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) {
-                    return const Text('Product not found');
+                    return const Text('Loading...');
                   }
 
                   final product = snapshot.data!;
-                  return Text(product.title);
+                  return Wrap(
+                    spacing: 40,
+                    children: [
+                      SizedBox(
+                        width: 500,
+                        child: Image.asset(product.asset),
+                      ),
+                      SizedBox(
+                        width: 500,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              product.title,
+                              style: const TextStyle(
+                                fontSize: 32,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            Text(
+                              product.priceFormatted,
+                              style: const TextStyle(fontSize: 24),
+                            ),
+                            const SizedBox(height: 24),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: const Text('Add to cart'),
+                            ),
+                            const SizedBox(height: 24),
+                            Text(product.description),
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
                 },
               ),
             ),
